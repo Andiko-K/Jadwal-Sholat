9b7da8723d3332404debecb487abeab5a7eb87c3
Andiko Putra <andikoputra27@gmail.com>
2024-04-01 23:36:26 +0700

    Adding fiqih_frame for front-end only

--- main.py
+++ main.py
@@ -56,32 +56,38 @@ kota_option.place(x=550, y = 137)
 subuh_label = CTT.CTkLabel(master = main_frame, text_color = '#000000', fg_color='#D6E4F6', width = 70.34, height = 21,
                               font = ('Montserrat', 15), text = 'Subuh', bg_color='#D6E4F6').place(x = 152.93, y = 184)
 subuh_time = CTT.CTkLabel(master = main_frame, text_color = '#000000', fg_color='#D6E4F6', width = 70.34, height = 24,
-                              font = ('Montserrat', 15), text = '23:59', bg_color='#D6E4F6').place(x = 152.93, y = 224)
+                              font = ('Montserrat', 15), text = '23:59', bg_color='#D6E4F6')
+subuh_time.place(x = 152.93, y = 224)
 
 terbit_label = CTT.CTkLabel(master = main_frame, text_color = '#000000', fg_color='#D6E4F6', width = 63.3, height = 21,
                               font = ('Montserrat', 15), text = 'Terbit', bg_color='#D6E4F6').place(x = 285.39, y = 184)
 terbit_time = CTT.CTkLabel(master = main_frame, text_color = '#000000', fg_color='#D6E4F6', width = 63.3, height = 21,
-                              font = ('Montserrat', 15), text = '23:59', bg_color='#D6E4F6').place(x = 285.39, y = 224)
+                              font = ('Montserrat', 15), text = '23:59', bg_color='#D6E4F6')
+terbit_time.place(x = 285.39, y = 224)
 
 dhuhur_label = CTT.CTkLabel(master = main_frame, text_color = '#000000', fg_color='#D6E4F6', width = 83.23, height = 21,
                               font = ('Montserrat', 15), text = 'Dhuhur', bg_color='#D6E4F6').place(x = 410.82, y = 184)
 dhuhur_time = CTT.CTkLabel(master = main_frame, text_color = '#000000', fg_color='#D6E4F6', width = 83.23, height = 21,
-                              font = ('Montserrat', 15), text = '23:59', bg_color='#D6E4F6').place(x = 410.82, y = 224)
+                              font = ('Montserrat', 15), text = '23:59', bg_color='#D6E4F6')
+dhuhur_time.place(x = 410.82, y = 224)
 
 ashar_label = CTT.CTkLabel(master = main_frame, text_color = '#000000', fg_color='#D6E4F6', width = 62.13, height = 21,
                               font = ('Montserrat', 15), text = 'Ashar', bg_color='#D6E4F6').place(x = 556.18, y = 184)
 ashar_time = CTT.CTkLabel(master = main_frame, text_color = '#000000', fg_color='#D6E4F6', width = 62.13, height = 21,
-                              font = ('Montserrat', 15), text = '23:59', bg_color='#D6E4F6').place(x = 556.18, y = 224)
+                              font = ('Montserrat', 15), text = '23:59', bg_color='#D6E4F6')
+ashar_time.place(x = 556.18, y = 224)
 
 maghrib_label = CTT.CTkLabel(master = main_frame, text_color = '#000000', fg_color='#D6E4F6', width = 91.44, height = 21,
                               font = ('Montserrat', 15), text = 'Maghrib', bg_color='#D6E4F6').place(x = 680.44, y = 184)
 maghrib_time= CTT.CTkLabel(master = main_frame, text_color = '#000000', fg_color='#D6E4F6', width = 91.44, height = 21,
-                              font = ('Montserrat', 15), text = '23:59', bg_color='#D6E4F6').place(x = 680.44, y = 224)
+                              font = ('Montserrat', 15), text = '23:59', bg_color='#D6E4F6')
+maghrib_time.place(x = 680.44, y = 224)
 
 isya_label = CTT.CTkLabel(master = main_frame, text_color = '#000000', fg_color='#D6E4F6', width = 41.03, height = 21,
                               font = ('Montserrat', 15), text = 'Isya', bg_color='#D6E4F6').place(x = 824.01, y = 184)
 isya_time = CTT.CTkLabel(master = main_frame, text_color = '#000000', fg_color='#D6E4F6', width = 41.03, height = 21,
-                              font = ('Montserrat', 15), text = '23:59', bg_color='#D6E4F6').place(x = 824.01, y = 224)
+                              font = ('Montserrat', 15), text = '23:59', bg_color='#D6E4F6')
+isya_time.place(x = 824.01, y = 224)
 
 ## line----------------------------------------------------------------------------------------------------------------------------------
 horizontal_line = CTT.CTkFrame(master = main_frame, bg_color = '#000000', width = 693.98,
@@ -119,7 +125,7 @@ param_coordinate = CTT.CTkLabel(master = main_frame, fg_color = '#D8E1E8', width
                         text = f"Koordinat: {coordinate}", anchor = 'w').place(x=765.52, y = 330)
 param_altitude = CTT.CTkLabel(master = main_frame, fg_color = '#D8E1E8', width = 247.95, height = 12.99,
                         text_color = '#000000', font = ('Montserrat', 13), bg_color= '#D8E1E8',
-                        text = f"Ketinggian: {altitude} MDPL", anchor = 'w').place(x=765.52, y = 357)
+                        text = f"Ketinggian: {altitude} MDPL", anchor = 'w').place(x=765.52, y = 357 )
 timezone = CTT.CTkLabel(master = main_frame, fg_color = '#D8E1E8', width = 247.95, height = 12.99,
                         text_color = '#000000', font = ('Montserrat', 13), bg_color= '#D8E1E8',
                         text = f'Zona Waktu: GMT+0{timezone}', anchor = 'w').place(x=765.52, y = 385)
@@ -127,6 +133,41 @@ timezone = CTT.CTkLabel(master = main_frame, fg_color = '#D8E1E8', width = 247.9
 fiqih_frame = CTT.CTkFrame(master = main_frame, fg_color = '#D8E1E8', width = 261,
                            height = 163, corner_radius=15)
 fiqih_frame.place(x=759, y = 433)
+fiqih_text = CTT.CTkLabel(master = main_frame, fg_color = '#D8E1E8', width = 163.12, height = 30.32,
+                        text_color = '#000000', font = ('Montserrat', 15, 'underline'), bg_color= '#D8E1E8',
+                        text = 'Ketetapan Fiqih').place(x=811.2, y = 440.26)
+
+koreksi_dhuhur_text = CTT.CTkLabel(master = main_frame, fg_color = '#D8E1E8', text_color = '#000000', bg_color = '#D8E1E8',
+                            text = 'Koreksi Waktu Dhuhur:', font = ('Montserrat', 12)).place(x = 764, y = 467)
+koreksi_dhuhur_option = CTT.CTkOptionMenu(master = main_frame, fg_color = '#D8E1E8', button_color='#D8E1E8', width = 50,
+                                     font = ('Montserrat', 12), dropdown_font=('Montserrat', 10), 
+                                    corner_radius= 0, bg_color='#D6E4F6', text_color='#000000', anchor = 'e')
+koreksi_dhuhur_option.set('10')
+koreksi_dhuhur_option.place(x = 967, y = 467)
+
+bayangan_ashar_text = CTT.CTkLabel(master = main_frame, fg_color = '#D8E1E8', text_color = '#000000', bg_color = '#D8E1E8',
+                            text = 'Tetapan Ashar:', font = ('Montserrat', 12)).place(x = 764, y = 497)
+bayangan_ashar_option = CTT.CTkOptionMenu(master = main_frame, fg_color = '#D8E1E8', button_color='#D8E1E8', width = 50,
+                                     font = ('Montserrat', 12), dropdown_font=('Montserrat', 10), 
+                                    corner_radius= 0, bg_color='#D6E4F6', text_color='#000000', anchor = 'e')
+bayangan_ashar_option.set("Syafi'i(+1)")
+bayangan_ashar_option.place(x = 929, y = 497)
+
+ketinggian_shubuh_text = CTT.CTkLabel(master = main_frame, fg_color = '#D8E1E8', text_color = '#000000', bg_color = '#D8E1E8',
+                            text = 'Ketinggian Shubuh:', font = ('Montserrat', 12)).place(x = 764, y = 527)
+ketinggian_shubuh_option = CTT.CTkOptionMenu(master = main_frame, fg_color = '#D8E1E8', button_color='#D8E1E8', width = 50,
+                                     font = ('Montserrat', 12), dropdown_font=('Montserrat', 10), 
+                                    corner_radius= 0, bg_color='#D6E4F6', text_color='#000000', anchor = 'e')
+ketinggian_shubuh_option.set("18°")
+ketinggian_shubuh_option.place(x = 962, y = 527)
+
+ketinggian_isya_text = CTT.CTkLabel(master = main_frame, fg_color = '#D8E1E8', text_color = '#000000', bg_color = '#D8E1E8',
+                            text = 'Ketinggian Isya:', font = ('Montserrat', 13)).place(x = 764, y = 557)
+ketinggian_isya_option = CTT.CTkOptionMenu(master = main_frame, fg_color = '#D8E1E8', button_color='#D8E1E8', width = 50,
+                                     font = ('Montserrat', 12), dropdown_font=('Montserrat', 10), 
+                                    corner_radius= 0, bg_color='#D6E4F6', text_color='#000000', anchor = 'e')
+ketinggian_isya_option.set("18°")
+ketinggian_isya_option.place(x = 962, y = 557)
 
 #tabel_frame
 tabel_frame = CTT.CTkFrame(master = main_frame, fg_color = '#B2CBDE', width = 730,
